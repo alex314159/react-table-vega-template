300edf6af7be542f4a29741507d36fe7c5263e4a
alex <alexandre.almosni@gmail.com>
2020-09-20 12:19:26 +0100

    remove unnecessary code

--- src/main/app.cljs
+++ src/main/app.cljs
@@ -7,22 +7,8 @@
                 [vegacharttemplates :as charts]))
 
 
-(defn play-data [& names]
-  (for [n names
-        i (range 20)]
-    {:time i :item n :quantity (+ (Math/pow (* i (count n)) 0.8) (rand-int (count n)))}))
-
-(def line-plot
-  {:data {:values (play-data "monkey" "slipper" "broom")}
-   :encoding {:x {:field "time" :type "quantitative"}
-              :y {:field "quantity" :type "quantitative"}
-              :color {:field "item" :type "nominal"}}
-   :mark "line"})
-
-;; Render the plot
 
 (defn some-component []
-  (charts/cases-and-deaths-bar)
       [:div
        [:h1 "react-table v6 examples"]
        [:a {:href "https://www.npmjs.com/package/react-table-v6"} "npmjs link with docs"]
